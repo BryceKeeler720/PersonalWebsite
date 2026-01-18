248bc5de10f98cd72faed3f81a8037cab64d1d8d
BryceKeeler720 <bryce.keeler720@gmail.com>
2026-01-17 23:08:20 -0600

    cleaning up repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [mdx(), sitemap(), react()],
+    integrations: [react()],
 });
